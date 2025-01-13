1786f2d826ebbc7f0c7a8026789b50f442860fa4
Arladusk <Simonchristensen@live.se>
2025-01-13 21:08:57 +0100

    Added Clock, limits fps to 60, and keeps track of time since last .tick() was called in variable dt. Also some comments

--- main.py
+++ main.py
@@ -1,25 +1,31 @@
 import pygame
 from constants import SCREEN_WIDTH, SCREEN_HEIGHT, ASTEROID_MIN_RADIUS, ASTEROID_KINDS, ASTEROID_SPAWN_RATE, ASTEROID_MAX_RADIUS
 
+# Fills the game window with Black color. 
 def fill_screen(screen: pygame.Surface):
     black = (0, 0, 0)
     screen.fill(black) 
 
-def main_loop(screen: pygame.Surface):
+# To be written
+def main_loop(screen: pygame.Surface, clock: pygame.time.Clock):
+        dt = 0
         while True:
             for event in pygame.event.get():
                 if event.type ==pygame.QUIT:
                     return
             fill_screen(screen)
             pygame.display.flip()
+            dt = clock.tick(60) / 1000
 
+# Initiates pygame, creates a Display and Clock object, prints some terminal text, starts the game. 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
     print("Starting asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
-    main_loop(screen)
+    main_loop(screen, clock)
 
 if __name__ == "__main__":
     main()
